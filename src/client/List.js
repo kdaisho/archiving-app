56732df7d0a21652ab0bc426b1a02eaeac78b516
daisho <daishokomiyama@gmail.com>
2020-02-16 21:28:30 -0500

    Adding modal

--- src/client/List.js
+++ src/client/List.js
@@ -1,100 +1,146 @@
-import React from "react";
+import React, { Component } from "react";
 
-const List = ({
-    langList,
-    searchTerm,
-    sortAl,
-    loading,
-    deleteOne,
-    editOne
-}) => {
-    let total = 0;
-    const currentPort = window.location.href.replace(
-        /https?:\/\/localhost:(\d{4})\//,
-        "$1"
-    );
-    const filePathDev = "./dist/images/uploads/";
-    const filePathProd = "./images/uploads/";
+class List extends Component {
+    state = {
+        imageName: "",
+        showModal: false
+    };
 
-    return (
-        <table className="table is-bordered is-striped">
-            <thead>
-                <tr className="has-background-warning">
-                    <th>Index</th>
-                    <th>Language</th>
-                    <th>Framework</th>
-                    <th>Logo</th>
-                    <th>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-                {langList
-                    .sort((a, b) => {
-                        return sortAl
-                            ? a.name.localeCompare(b.name)
-                            : a.id - b.id;
-                    })
-                    .map(lang =>
-                        lang.frameworks
-                            .filter(
-                                fw =>
-                                    `${fw.name}`
-                                        .toLowerCase()
-                                        .indexOf(searchTerm) >= 0
-                            )
-                            .map(fw => {
-                                total += 1;
-                                return (
-                                    <tr key={fw.id}>
-                                        <td>{total}</td>
-                                        <td>{lang.name}</td>
-                                        <td>{fw.name}</td>
-                                        <td>
-                                            {loading ? (
-                                                "Loading..."
-                                            ) : (
-                                                <img
-                                                    className="framework-logo"
-                                                    src={`${
-                                                        currentPort === "3000"
-                                                            ? filePathDev
-                                                            : filePathProd
-                                                    }${fw.filename}`}
-                                                    alt={fw.name}
-                                                />
-                                            )}
-                                        </td>
-                                        <td>
-                                            <span className="action-button-container">
-                                                <button
-                                                    className="button is-warning"
-                                                    onClick={() =>
-                                                        editOne(fw.id)
-                                                    }
-                                                >
-                                                    Edit
-                                                </button>
-                                                <button
-                                                    className="button is-danger"
-                                                    onClick={() =>
-                                                        deleteOne(
-                                                            lang.name,
-                                                            fw.name,
-                                                            fw.filename
-                                                        )
-                                                    }
-                                                >
-                                                    Delete
-                                                </button>
-                                            </span>
-                                        </td>
-                                    </tr>
-                                );
+    handleDisplayImage = filename => {
+        this.setState({
+            imageName: filename,
+            showModal: !this.state.showModal
+        });
+    };
+
+    render() {
+        const {
+            langList,
+            searchTerm,
+            sortAl,
+            loading,
+            deleteOne,
+            editOne
+        } = this.props;
+
+        let total = 0;
+        const currentPort = window.location.href.replace(
+            /https?:\/\/localhost:(\d{4})\//,
+            "$1"
+        );
+        const filePathDev = "./dist/images/uploads/";
+        const filePathProd = "./images/uploads/";
+
+        return (
+            <div>
+                <div
+                    className={`modal ${this.state.showModal ? "active" : ""}`}
+                >
+                    <div
+                        className="modal-background"
+                        onClick={this.handleDisplayImage}
+                    ></div>
+                    <div className="modal-content">
+                        <img
+                            src={`${
+                                currentPort === "3000"
+                                    ? filePathDev
+                                    : filePathProd
+                            }${this.state.imageName}`}
+                            alt={this.state.imageName}
+                        />
+                    </div>
+                    <button
+                        className="modal-close is-large"
+                        onClick={this.handleDisplayImage}
+                    ></button>
+                </div>
+                <table className="table is-bordered is-striped">
+                    <thead>
+                        <tr className="has-background-warning">
+                            <th>Index</th>
+                            <th>Language</th>
+                            <th>Framework</th>
+                            <th>Logo</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {langList
+                            .sort((a, b) => {
+                                return sortAl
+                                    ? a.name.localeCompare(b.name)
+                                    : a.id - b.id;
                             })
-                    )}
-            </tbody>
-        </table>
-    );
-};
+                            .map(lang =>
+                                lang.frameworks
+                                    .filter(
+                                        fw =>
+                                            `${fw.name}`
+                                                .toLowerCase()
+                                                .indexOf(searchTerm) >= 0
+                                    )
+                                    .map(fw => {
+                                        total += 1;
+                                        return (
+                                            <tr key={fw.id}>
+                                                <td>{total}</td>
+                                                <td>{lang.name}</td>
+                                                <td>{fw.name}</td>
+                                                <td>
+                                                    {loading ? (
+                                                        "Loading..."
+                                                    ) : (
+                                                        <img
+                                                            className="framework-logo"
+                                                            onClick={() =>
+                                                                this.handleDisplayImage(
+                                                                    fw.filename
+                                                                )
+                                                            }
+                                                            src={`${
+                                                                currentPort ===
+                                                                "3000"
+                                                                    ? filePathDev
+                                                                    : filePathProd
+                                                            }${fw.filename}`}
+                                                            alt={fw.name}
+                                                        />
+                                                    )}
+                                                </td>
+                                                <td>
+                                                    <span className="action-button-container">
+                                                        <button
+                                                            className="button is-warning"
+                                                            onClick={() =>
+                                                                editOne(fw.id)
+                                                            }
+                                                        >
+                                                            Edit
+                                                        </button>
+                                                        <button
+                                                            className="button is-danger"
+                                                            onClick={() =>
+                                                                deleteOne(
+                                                                    lang.name,
+                                                                    fw.name,
+                                                                    fw.filename
+                                                                )
+                                                            }
+                                                        >
+                                                            Delete
+                                                        </button>
+                                                    </span>
+                                                </td>
+                                            </tr>
+                                        );
+                                    })
+                            )}
+                    </tbody>
+                </table>
+            </div>
+        );
+    }
+}
 
 export default List;
